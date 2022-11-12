54ee275edae465c3cd15d1e8f4e2918d14ab9626
Christopher Bennett <macncheezy00@gmail.com>
2022-11-11 22:40:42 -0600

    Basic patrolling for enemies

--- Assets/Scripts/EnemyMovementController.cs
+++ Assets/Scripts/EnemyMovementController.cs
@@ -4,11 +4,15 @@ using UnityEngine;
 
 public class EnemyMovementController : MonoBehaviour
 {
+    public bool IsPatrol;
     public Transform Target;
+    public List<Transform> PatrolPoints = new List<Transform>();
     public float Speed;
     public float StopDistance = 1.5f;
 
     EnemyStatsController StatsController;
+    int nextPatrolIndex;
+    Vector3 nextPatrolPoint;
 
     // Start is called before the first frame update
     void Awake()
@@ -16,8 +20,22 @@ public class EnemyMovementController : MonoBehaviour
         StatsController = GetComponent<EnemyStatsController>();
     }
 
+    private void Start()
+    {
+        nextPatrolIndex = 1;
+    }
+
     private void FixedUpdate()
     {
+        if (IsPatrol && StatsController.IsAlive())
+        {
+            nextPatrolPoint = PatrolPoints[nextPatrolIndex].position;
+            Vector3 direction = nextPatrolPoint - transform.position;
+            if (direction.sqrMagnitude > StopDistance)
+                transform.position += direction.normalized * Speed * Time.fixedDeltaTime;
+            else
+                nextPatrolIndex = (nextPatrolIndex + 1) % PatrolPoints.Count;
+        }
         if (Target && StatsController.IsAlive())
         {
             Vector3 direction = Target.position - transform.position;
